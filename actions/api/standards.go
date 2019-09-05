10676aa6002bd29304f52104933fbaca7a5e7f54
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-05 21:28:56 +0200

    Implement api/standards#show

--- actions/api/standards.go
+++ actions/api/standards.go
@@ -16,7 +16,14 @@ func (v StandardsResource) List(c buffalo.Context) error {
 	return c.Render(200, r.JSON((*ms).GetAllStandards()))
 }
 
-// show default implementation.
-func (v StandardsResource) show(c buffalo.Context) error {
-	return c.Render(200, r.String("Standard#show"))
+// Show default implementation.
+func (v StandardsResource) Show(c buffalo.Context) error {
+	ms := masonry.GetInstance()
+	standardID := c.Param("standard_id")
+	for _, s := range (*ms).GetAllStandards() {
+		if s.GetName() == standardID {
+			return c.Render(200, r.JSON(s))
+		}
+	}
+	return c.Render(404, r.JSON("Not found"))
 }
