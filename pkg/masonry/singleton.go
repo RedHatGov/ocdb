e4fa0972a5b44891a5149a92671329e3ba9e975e
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-05 16:14:00 +0200

    Implement method to fetch masonry cache to memory

---
+++ pkg/masonry/singleton.go
@@ -0,0 +1,26 @@
+package masonry
+
+import (
+	"github.com/opencontrol/compliance-masonry/pkg/lib"
+	"github.com/opencontrol/compliance-masonry/pkg/lib/common"
+	"sync"
+)
+
+var instance *common.Workspace
+var once sync.Once
+
+// GetInstance gets memory representation of the masonry cache
+func GetInstance() *common.Workspace {
+	once.Do(func() {
+		err := BuildCache()
+		if err != nil {
+			panic(err)
+		}
+		data, errors := lib.LoadData(".masonry_cache", ".masonry_cache/certifications/dhs-4300a.yaml")
+		if errors != nil {
+			panic(errors)
+		}
+		instance = &data
+	})
+	return instance
+}
