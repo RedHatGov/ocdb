b480e7d591db154c02c9b1f25106fc8c627719ba
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-05 16:17:34 +0200

    Dirty plug to current api controller to show case list of standards

--- actions/api.go
+++ actions/api.go
@@ -1,8 +1,12 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/isimluk/ocdb/pkg/masonry"
+)
 
 // APIHandler is a default handler to serve API
 func APIHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON("OK"))
+	ms := masonry.GetInstance()
+	return c.Render(200, r.JSON((*ms).GetAllStandards()))
 }
