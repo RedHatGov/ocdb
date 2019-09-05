3b5c3df658387086b0cacb96dd3c56eef4c58176
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-05 21:15:52 +0200

    Refactor Standards to have restful routes

--- actions/api/standards.go
+++ actions/api/standards.go
@@ -5,8 +5,18 @@ import (
 	"github.com/isimluk/ocdb/pkg/masonry"
 )
 
-// Handler is a default handler to serve API
-func Handler(c buffalo.Context) error {
+// StandardsResource show standards like defined by opencontrols
+type StandardsResource struct {
+	buffalo.Resource
+}
+
+// List default implementation.
+func (v StandardsResource) List(c buffalo.Context) error {
 	ms := masonry.GetInstance()
 	return c.Render(200, r.JSON((*ms).GetAllStandards()))
+}
+
+// show default implementation.
+func (v StandardsResource) show(c buffalo.Context) error {
+	return c.Render(200, r.String("Standard#show"))
 }
