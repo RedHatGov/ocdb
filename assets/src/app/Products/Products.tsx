02fc93184279db18313206cdec74512a829ba90f
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-19 15:02:22 +0200

    Products page should dynamically load products
    and for now it should print out at least number of products

--- assets/src/app/Products/Products.tsx
+++ assets/src/app/Products/Products.tsx
@@ -12,6 +12,7 @@ import {
   CardHead,
   CardHeader,
   TextVariants } from '@patternfly/react-core';
+import { Spinner } from '@patternfly/react-core/dist/esm/experimental';
 
 import nistLogo from '@app/assets/images/nist-logo-2x.png';
 import ansibleTowerLogo from '@app/assets/images/ansible-tower-logo.png';
@@ -20,56 +21,75 @@ import openshiftLogo from '@app/assets/images/openshift-logo.png';
 import openstackLogo from '@app/assets/images/openstack-logo.png';
 
 
-
-
 class Products extends React.Component {
     render(){
         return (
-          <Page>
-          <PageSection variant={PageSectionVariants.light}>
-            <TextContent>
-              <Text component="h1">Product Documents</Text>
-              <Text component="p">Product-specific security documentation.</Text>
-            </TextContent>
-          </PageSection>
-            <PageSection>
-              <Gallery gutter="md">
-                <GalleryItem>
-                  <Card isHoverable>
-                    <CardHead>
-                      <img src={ansibleTowerLogo} />
-                    </CardHead>
-                    <CardHeader>Ansible Tower</CardHeader>
-                  </Card>
-                </GalleryItem>
-                <GalleryItem>
-                  <Card isHoverable>
-                    <CardHead>
-                      <img src={coreosLogo} />
-                    </CardHead>
-                    <CardHeader>CoreOS 4.x</CardHeader>
-                  </Card>
-                </GalleryItem>
-                <GalleryItem>
-                  <Card isHoverable>
-                    <CardHead>
-                      <img src={openshiftLogo} />
-                    </CardHead>
-                    <CardHeader>OpenShift 3.x</CardHeader>
-                  </Card>
-                </GalleryItem>
-                <GalleryItem>
-                  <Card isHoverable>
-                    <CardHead>
-                      <img src={openstackLogo} />
-                    </CardHead>
-                    <CardHeader>OpenStack 13</CardHeader>
-                  </Card>
-                </GalleryItem>
-              </Gallery>
-            </PageSection>
-          </Page>
+            <Page>
+                <PageSection variant={PageSectionVariants.light}>
+                    <TextContent>
+                        <Text component="h1">Product Documents</Text>
+                        <Text component="p">Product-specific security documentation.</Text>
+                    </TextContent>
+                </PageSection>
+
+                <PageSection>
+                    <Gallery gutter="md">
+                        { (this.state['isLoading'] ? <Spinner/> : this.state['products'].map((function(object, i){
+                            return (<Text component="p">{i}</Text>);
+                        })))}
+                    </Gallery>
+                </PageSection>
+
+                <PageSection>
+                    <Gallery gutter="md">
+                        <GalleryItem>
+                            <Card isHoverable>
+                                <CardHead>
+                                    <img src={ansibleTowerLogo} />
+                                </CardHead>
+                                <CardHeader>Ansible Tower</CardHeader>
+                            </Card>
+                        </GalleryItem>
+                        <GalleryItem>
+                            <Card isHoverable>
+                                <CardHead>
+                                    <img src={coreosLogo} />
+                                </CardHead>
+                                <CardHeader>CoreOS 4.x</CardHeader>
+                            </Card>
+                        </GalleryItem>
+                        <GalleryItem>
+                            <Card isHoverable>
+                                <CardHead>
+                                    <img src={openshiftLogo} />
+                                </CardHead>
+                                <CardHeader>OpenShift 3.x</CardHeader>
+                            </Card>
+                        </GalleryItem>
+                        <GalleryItem>
+                            <Card isHoverable>
+                                <CardHead>
+                                    <img src={openstackLogo} />
+                                </CardHead>
+                                <CardHeader>OpenStack 13</CardHeader>
+                            </Card>
+                        </GalleryItem>
+                    </Gallery>
+                </PageSection>
+            </Page>
         );
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            products: []
+        };
+        fetch('/api/v1/components')
+            .then(response => response.json())
+            .then(data => this.setState({products: data, isLoading: false}));
     }
 }
 
