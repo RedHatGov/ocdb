7a4b14a7f7262a59f155e1b75743a91feffec347
Šimon Lukašík <isimluk@fedoraproject.org>
2019-09-22 11:10:14 +0200

    Visualize content on the products page a bit

--- assets/src/app/Products/Product.tsx
+++ assets/src/app/Products/Product.tsx
@@ -6,9 +6,66 @@ import {
   TextContent,
   Text,
 } from '@patternfly/react-core';
+import { IRow, Table, TableBody, TableHeader, TableVariant,} from '@patternfly/react-table'
 import { Spinner } from '@patternfly/react-core/dist/esm/experimental';
 
+export interface RTMProps {
+    content: string;
+    groupName: string;
+}
+
+export interface RTMState {
+    columns: string[];
+    rows: (string[] | IRow)[];
+}
+
+class RTM extends React.Component<RTMProps, RTMState> {
+    constructor(props) {
+        super(props);
+
+        var rows = props.content.map(function(c, _idx) {
+            console.log(c)
+            var implementation_status = c.Satisfies ? c.Satisfies.implementation_status : "unknown";
+            return {cells: [c.Key, c.Control.name, implementation_status]};
+        })
+
+        this.state = {
+            columns: [
+                'Control', 'Name', 'Status'
+            ],
+            rows: rows,
+        }
+    }
+
+    render(){
+        return (
+            <TextContent>
+                <Text component="h3">{this.props.groupName}</Text>
+                <Table caption="Requirements Traceability Matrix"
+                        variant={TableVariant.compact}
+                        rows={this.state.rows}
+                        cells={this.state.columns}>
+                    <TableHeader />
+                    <TableBody />
+                </Table>
+            </TextContent>
+        )
+    }
+}
+
 class Product extends React.Component {
+    renderControls() {
+        var controls = this.state['product']['controls'];
+        var nist80053 = controls['NIST-800-53'];
+        return (
+            <TextContent>
+            { Object.keys(nist80053).map(function(k, i) {
+                return (<RTM key={k} groupName={k} content={nist80053[k]}/>);
+            })}
+            </TextContent>
+        )
+    }
+
     render(){
         return (
             <Page>
@@ -20,7 +77,7 @@ class Product extends React.Component {
                           <Text component="h2">Product-specific security documentation.</Text>
                           <Text component="p">TBD lorem.</Text>
                           <Text component="h2">OpenControls</Text>
-                          <Text component="p">{JSON.stringify(this.state['product']['controls'])}</Text>
+                          { this.renderControls() }
                           { this.state['product']['errors'].length == 0 ? ' ' : <Alert  variant="warning" title="Minor problem found">{this.state['product']['errors']}</Alert> }
                       </TextContent>
                     }
