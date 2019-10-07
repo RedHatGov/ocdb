5010c8fd2f981a940f8dff1cfc2b5cf638b742a8
Šimon Lukašík <isimluk@fedoraproject.org>
2019-10-07 21:23:40 +0200

    Wall of text looks better when there is blank column at the edge
    Wrapping by flex to achieve reasonable behavior on small devices.

--- assets/src/app/ato/Documents/Documents.tsx
+++ assets/src/app/ato/Documents/Documents.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Page, PageSection, PageSectionVariants } from '@patternfly/react-core';
+import { Flex, FlexItem } from '@patternfly/react-core';
+
 import VMP from '@app/assets/markdown/vulnerability-management-plan.md';
 import TP from '@app/assets/markdown/training-plan.md';
 import { Markdown } from '@app/lib/markdown';
@@ -17,12 +19,24 @@ const ATOVulnerabilityManagementPlan: React.FunctionComponent<any> = (props) => 
 }
 
 const ATOTrainingPlan: React.FunctionComponent<any> = (props) => {
+    var textMod = {modifier: 'flex-2' as 'flex-default', breakpoint: 'md' as 'md'};
+    var emptyMod = {modifier: 'flex-1' as 'flex-default', breakpoint: 'md' as 'md'};
     return (
         <Page>
             <PageSection variant={PageSectionVariants.light}>
-                <Markdown>
-                    <TP/>
-                </Markdown>
+                <Flex>
+                    <Flex breakpointMods={[textMod]}>
+                        <FlexItem>
+                            <Markdown>
+                                <TP/>
+                            </Markdown>
+                        </FlexItem>
+                    </Flex>
+
+                    <Flex breakpointMods={[emptyMod]}>
+                        <FlexItem/>
+                    </Flex>
+                </Flex>
             </PageSection>
         </Page>
     );
