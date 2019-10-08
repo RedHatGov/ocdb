5eb1ecac16ff36bbb73b05227924ad2f3ef10528
Šimon Lukašík <isimluk@fedoraproject.org>
2019-10-08 13:26:25 +0200

    Refactor: Extract component: Document

--- assets/src/app/ato/Documents/Documents.tsx
+++ assets/src/app/ato/Documents/Documents.tsx
@@ -6,19 +6,7 @@ import VMP from '@app/assets/markdown/vulnerability-management-plan.md';
 import TP from '@app/assets/markdown/training-plan.md';
 import { Markdown } from '@app/lib/markdown';
 
-const ATOVulnerabilityManagementPlan: React.FunctionComponent<any> = (props) => {
-    return (
-    <Page>
-      <PageSection variant={PageSectionVariants.light}>
-          <Markdown>
-                <VMP/>
-          </Markdown>
-      </PageSection>
-    </Page>
-    );
-}
-
-const ATOTrainingPlan: React.FunctionComponent<any> = (props) => {
+const Document: React.FunctionComponent<any> = (props) => {
     var textMod = {modifier: 'flex-2' as 'flex-default', breakpoint: 'md' as 'md'};
     var emptyMod = {modifier: 'flex-1' as 'flex-default', breakpoint: 'md' as 'md'};
     return (
@@ -28,18 +16,25 @@ const ATOTrainingPlan: React.FunctionComponent<any> = (props) => {
                     <Flex breakpointMods={[textMod]}>
                         <FlexItem>
                             <Markdown>
-                                <TP/>
+                                {props.children}
                             </Markdown>
                         </FlexItem>
                     </Flex>
-
                     <Flex breakpointMods={[emptyMod]}>
                         <FlexItem/>
                     </Flex>
                 </Flex>
             </PageSection>
         </Page>
-    );
+    )
+}
+
+const ATOVulnerabilityManagementPlan: React.FunctionComponent<any> = (props) => {
+    return (<Document><VMP/></Document>);
+}
+
+const ATOTrainingPlan: React.FunctionComponent<any> = (props) => {
+    return (<Document><TP/></Document>);
 }
 
 export { ATOVulnerabilityManagementPlan, ATOTrainingPlan };
