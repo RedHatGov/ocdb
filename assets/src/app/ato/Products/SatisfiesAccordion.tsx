c95b521211acec3013c79e92025ea55897782eb2
Šimon Lukašík <isimluk@fedoraproject.org>
2019-11-27 16:06:44 +0100

    Whitespace chages

--- assets/src/app/ato/Products/SatisfiesAccordion.tsx
+++ assets/src/app/ato/Products/SatisfiesAccordion.tsx
@@ -8,26 +8,26 @@ interface CustomControlProps {
 }
 
 const SatisfiesAccordion: React.FunctionComponent<CustomControlProps> = (props) => {
-        if (props.satisfies == null || (props.satisfies.narrative.length == 1 && props.satisfies.narrative[0].text == '')) {
-            return (<Text component="p">Not available</Text>);
-        }
-        if (props.satisfies.narrative.length == 1 && props.satisfies.narrative[0].key == undefined) {
-            return (<MDX>{props.satisfies.narrative[0].text}</MDX>)
-        }
+    if (props.satisfies == null || (props.satisfies.narrative.length == 1 && props.satisfies.narrative[0].text == '')) {
+        return (<Text component="p">Not available</Text>);
+    }
+    if (props.satisfies.narrative.length == 1 && props.satisfies.narrative[0].key == undefined) {
+        return (<MDX>{props.satisfies.narrative[0].text}</MDX>)
+    }
 
-        const cKey = props.satisfies.control_key;
-        return (
-            <React.Fragment>
-                { props.satisfies.narrative.map(function(n, idx) {
-                    return (
-                <Expandable key={idx} toggleText={cKey + '(' + n.key + ')'} isExpanded>
-                    <MDX>{n.text}</MDX>
-                    <br/>
-                </Expandable>
-                    )
-                })}
-            </React.Fragment>
-        )
+    const cKey = props.satisfies.control_key;
+    return (
+        <React.Fragment>
+            { props.satisfies.narrative.map(function(n, idx) {
+                return (
+                    <Expandable key={idx} toggleText={cKey + '(' + n.key + ')'} isExpanded>
+                        <MDX>{n.text}</MDX>
+                        <br/>
+                    </Expandable>
+                )
+            })}
+        </React.Fragment>
+    )
 }
 
 export { SatisfiesAccordion }
