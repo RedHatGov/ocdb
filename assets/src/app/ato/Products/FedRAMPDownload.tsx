a4e79fa891d182a64cda9d3fb69bfa5da106f72c
Šimon Lukašík <isimluk@fedoraproject.org>
2019-12-10 17:57:14 +0100

    Rewrite Fedramp download component to show up as table instead of modal

--- assets/src/app/ato/Products/FedRAMPDownload.tsx
+++ assets/src/app/ato/Products/FedRAMPDownload.tsx
@@ -1,80 +1,76 @@
 import * as React from 'react';
 import {
-    Button,
-    Modal,
+    DataList,
+    DataListItem,
+    DataListItemRow,
+    DataListCell,
+    DataListItemCells,
     Text,
+    TextContent,
 } from '@patternfly/react-core';
 import { FileWordIcon } from '@patternfly/react-icons'
 
 import { ProductInfo } from '@app/ato/Products/Static.tsx'
 
-
 interface FedRAMPDownloadProp {
     productId: string;
 }
 
-interface FedRAMPDownloadState {
-    isModalOpen: boolean;
-}
-
-class FedRAMPDownload extends React.PureComponent<FedRAMPDownloadProp, FedRAMPDownloadState> {
-    handleModalToggle() {
-        this.setState(({ isModalOpen }) => ({
-            isModalOpen: !isModalOpen
-        }));
-    }
-    onSubmit(level) {
-        this.handleModalToggle();
-        window.location.assign('/api/v1/components/' + this.props.productId + '/fedramp/' + level);
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = { isModalOpen: false };
-        this.handleModalToggle = this.handleModalToggle.bind(this);
-    }
-
+class FedRAMPDownload extends React.PureComponent<FedRAMPDownloadProp> {
   render() {
     if (ProductInfo[this.props.productId] && ProductInfo[this.props.productId].disableFedrampDownload) {
-        return '';
+        return <Text component='p'>FedRAMP for {this.props.productId} is not available at this time</Text>
     }
-
-    const { isModalOpen } = this.state;
-    const onSubmitHigh = this.onSubmit.bind(this, "High");
-    const onSubmitModerate = this.onSubmit.bind(this, "Moderate");
-    const onSubmitLow = this.onSubmit.bind(this, "Low");
+      const productId = this.props.productId;
 
     return (
         <React.Fragment>
-            <Button variant="link" onClick={this.handleModalToggle} icon={<FileWordIcon/>} >
-                Download FedRAMP Template
-            </Button>
+            <TextContent>
+                <Text component="h1">FedRAMP Templates</Text>
+                <Text component="p">
+                    The FedRAMP template is dynamically generated using the <Text component="a" href="https://github.com/opencontrol/fedramp-templater">OpenControl FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text>. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramp.gov/templates/">provided by the GSA FedRAMP PMO</Text>.
+                </Text>
+            </TextContent>
+
             <br/>
-            <br/>
-            <Modal
-                isLarge
-                title="FedRAMP Template"
-                isOpen={isModalOpen}
-                onClose={this.handleModalToggle}
-                actions={[
-                    <Button key="confirm" variant="primary" onClick={onSubmitHigh}>
-                        <FileWordIcon/> Fedramp High
-                    </Button>,
-                    <Button key="confirm2" variant="primary" onClick={onSubmitModerate}>
-                        <FileWordIcon/> Fedramp Moderate
-                    </Button>,
-                    <Button key="confirm3" variant="primary" onClick={onSubmitLow}>
-                        <FileWordIcon/> Fedramp Low
-                    </Button>,
-                    <Button key="cancel" variant="link" onClick={this.handleModalToggle}>
-                        Cancel
-                    </Button>
-                ]}
-                appendTo={document.body}
-                isFooterLeftAligned
-            >
-                The FedRAMP template is dynamically generated using the <Text component="a" href="https://github.com/opencontrol/fedramp-templater">OpenControl FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text>. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramp.gov/templates/">provided by the GSA FedRAMP PMO</Text>.
-            </Modal>
+            <DataList aria-label="Simple data list example">
+                <DataListItem aria-labelledby="low">
+                    <DataListItemRow>
+                        <DataListItemCells dataListCells={[
+                            <DataListCell key="title">
+                                FedRAMP Low
+                            </DataListCell>,
+                            <DataListCell key="link">
+                                <Text component="a" href={'/api/v1/components/' + productId + '/fedramp/Low'}><FileWordIcon/> .docx</Text>
+                            </DataListCell>,
+                        ]}/>
+                    </DataListItemRow>
+                </DataListItem>
+                <DataListItem aria-labelledby="moderate">
+                    <DataListItemRow>
+                        <DataListItemCells dataListCells={[
+                            <DataListCell>
+                                FedRAMP Moderate
+                            </DataListCell>,
+                            <DataListCell>
+                                <Text component="a" href={'/api/v1/components/' + productId + '/fedramp/Moderate'}><FileWordIcon/> .docx</Text>
+                            </DataListCell>,
+                        ]}/>
+                    </DataListItemRow>
+                </DataListItem>
+                <DataListItem aria-labelledby="high">
+                    <DataListItemRow>
+                        <DataListItemCells dataListCells={[
+                            <DataListCell>
+                                FedRAMP High
+                            </DataListCell>,
+                            <DataListCell>
+                                <Text component="a" href={'/api/v1/components/' + productId + '/fedramp/High'}><FileWordIcon/> .docx</Text>
+                            </DataListCell>,
+                        ]}/>
+                    </DataListItemRow>
+                </DataListItem>
+            </DataList>
         </React.Fragment>
     );
   }
