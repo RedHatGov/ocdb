9c1a3253bd2a5a67ddf422f44ba588dab8d2d68f
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-10 12:51:41 +0100

    log errors from buffalo worker methods

--- actions/jobs/job.go
+++ actions/jobs/job.go
@@ -3,6 +3,7 @@ package jobs
 import (
 	"github.com/RedHatGov/ocdb/pkg/cac"
 	"github.com/RedHatGov/ocdb/pkg/masonry"
+	"github.com/RedHatGov/ocdb/pkg/utils"
 	"github.com/gobuffalo/buffalo/worker"
 	"time"
 )
@@ -15,17 +16,22 @@ func Init(w worker.Worker) {
 }
 
 func setUpJob(w worker.Worker, name string, job JobFn) {
-	w.Register(name, func(args worker.Args) error {
+	err := w.Register(name, func(args worker.Args) error {
 		rescheduleJob(w, name, time.Hour)
 		return job()
 	})
+	if err != nil {
+		utils.Log.Fatalf("Could not register job: %v", err)
+	}
 	rescheduleJob(w, name, time.Minute)
 }
 
 func rescheduleJob(w worker.Worker, handlerName string, period time.Duration) {
-	w.PerformIn(worker.Job{
+	err := w.PerformIn(worker.Job{
 		Queue:   "masonry",
 		Handler: handlerName,
 	}, period)
-
+	if err != nil {
+		utils.Log.Errorf("Could not reschedule job: %v", err)
+	}
 }
