569286d84da14424c3639e7ebb793a8902fd1491
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-10 11:48:48 +0100

    Refactor: Move jobs manipulation to separate module
    This is probably gonna grow in size. Better to have this separated.

---
+++ actions/jobs/job.go
@@ -0,0 +1,32 @@
+package jobs
+
+import (
+	"github.com/RedHatGov/ocdb/pkg/cac"
+	"github.com/RedHatGov/ocdb/pkg/masonry"
+	"github.com/gobuffalo/buffalo/worker"
+	"time"
+)
+
+type JobFn func()
+
+func Init(w worker.Worker) {
+	setUpJob(w, "refresh_masonry", masonry.Refresh)
+	setUpJob(w, "refresh_cac", cac.Refresh)
+}
+
+func setUpJob(w worker.Worker, name string, job JobFn) {
+	w.Register(name, func(args worker.Args) error {
+		rescheduleJob(w, name, time.Hour)
+		job()
+		return nil
+	})
+	rescheduleJob(w, name, time.Minute)
+}
+
+func rescheduleJob(w worker.Worker, handlerName string, period time.Duration) {
+	w.PerformIn(worker.Job{
+		Queue:   "masonry",
+		Handler: handlerName,
+	}, period)
+
+}
--- actions/workers.go
+++ actions/workers.go
@@ -1,34 +1,10 @@
 package actions
 
 import (
-	"github.com/RedHatGov/ocdb/pkg/cac"
-	"github.com/RedHatGov/ocdb/pkg/masonry"
-	"github.com/gobuffalo/buffalo/worker"
-	"time"
+	"github.com/RedHatGov/ocdb/actions/jobs"
 )
-
-type JobFn func()
 
 func init() {
 	w := App().Worker
-	SetUpJob(w, "refresh_masonry", masonry.Refresh)
-	SetUpJob(w, "refresh_cac", cac.Refresh)
-}
-
-func SetUpJob(w worker.Worker, name string, job JobFn) {
-	w.Register(name, func(args worker.Args) error {
-		RescheduleJob(name, time.Hour)
-		job()
-		return nil
-	})
-	RescheduleJob(name, time.Minute)
-}
-
-func RescheduleJob(handlerName string, period time.Duration) {
-	w := App().Worker
-	w.PerformIn(worker.Job{
-		Queue:   "masonry",
-		Handler: handlerName,
-	}, period)
-
+	jobs.Init(w)
 }
