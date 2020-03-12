b19912d25f28a36f88509c724c9010cf69958816
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-12 09:05:45 +0100

    Move logical view computation to masonry
    Let's keep API lightweight. This code is concerning opencontrols only anyway.

--- actions/api/components.go
+++ actions/api/components.go
@@ -2,12 +2,9 @@ package api
 
 import (
 	"bytes"
-	"fmt"
-
 	"github.com/RedHatGov/ocdb/pkg/fedramp"
 	"github.com/RedHatGov/ocdb/pkg/masonry"
 	"github.com/gobuffalo/buffalo"
-	"github.com/opencontrol/compliance-masonry/pkg/lib/common"
 )
 
 // ComponentsResource show components like defined by opencontrols
@@ -31,89 +28,12 @@ func (v ComponentsResource) Show(c buffalo.Context) error {
 	return c.Render(404, r.JSON("Not found"))
 }
 
-// CustomControl is object that ties together information from standard with product specific "satisfaction" description
-type CustomControl struct {
-	Key       string
-	Control   common.Control
-	Satisfies common.Satisfies
-}
-
-func standardToLogicalView(s common.Standard) map[string][]CustomControl {
-	result := make(map[string][]CustomControl)
-	controls := s.GetControls()
-	for _, controlName := range s.GetSortedControls() {
-		control := controls[controlName]
-		_, ok := result[control.GetFamily()]
-		if !ok {
-			result[control.GetFamily()] = make([]CustomControl, 0)
-		}
-		result[control.GetFamily()] = append(result[control.GetFamily()], CustomControl{
-			Key:     controlName,
-			Control: control})
-	}
-	return result
-}
-
-var validImplementationStatuses = []string{"complete", "partial", "not applicable", "planned", "unsatisfied", "unknown", "none"}
-
-func logicalView(ms *masonry.OpencontrolData, c common.Component) (map[string]map[string][]CustomControl, []string) {
-	result := make(map[string]map[string][]CustomControl)
-	problems := make([]string, 0)
-
-	for _, satisfy := range c.GetAllSatisfies() {
-		standardKey := satisfy.GetStandardKey()
-		_, ok := result[standardKey]
-		if !ok {
-			standard, found := ms.GetStandard(standardKey)
-			if found {
-				result[standardKey] = standardToLogicalView(standard)
-			}
-
-		}
-		found := false
-		for groupId, group := range result[standardKey] {
-			for i, cc := range group {
-				if cc.Key == satisfy.GetControlKey() {
-					if cc.Satisfies != nil {
-						problems = append(problems, fmt.Sprintf("Found duplicate item: %s", cc.Key))
-					}
-
-					result[standardKey][groupId][i].Satisfies = satisfy
-					found = true
-
-					switch satisfy.GetImplementationStatus() {
-					case "complete", "partial", "not applicable", "planned", "unsatisfied", "unknown", "none":
-						break
-					default:
-						problems = append(problems,
-							fmt.Sprintf("Found non-standard implementation_status: %s. Expected one of %s.",
-								satisfy.GetImplementationStatus(), validImplementationStatuses))
-						break
-					}
-
-					break
-				}
-
-			}
-			if found {
-				break
-			}
-		}
-		if !found {
-			problems = append(problems, fmt.Sprintf("Could not find reference %s in the standard %s", satisfy.GetControlKey(), standardKey))
-
-		}
-	}
-
-	return result, problems
-}
-
 // ComponentControlsHandler gives logical human readable view of open control items available.
 func ComponentControlsHandler(c buffalo.Context) error {
 	ms := masonry.GetInstance()
 	component, found := ms.GetComponent(c.Param("component_id"))
 	if found {
-		lv, problems := logicalView(ms, component)
+		lv, problems := ms.ComponentLogicalView(component)
 		result := make(map[string]interface{})
 		result["name"] = component.GetName()
 		result["controls"] = lv
---
+++ pkg/masonry/logical_view.go
@@ -0,0 +1,83 @@
+package masonry
+
+import (
+	"fmt"
+	"github.com/opencontrol/compliance-masonry/pkg/lib/common"
+)
+
+// CustomControl is object that ties together information from standard with product specific "satisfaction" description
+type CustomControl struct {
+	Key       string
+	Control   common.Control
+	Satisfies common.Satisfies
+}
+
+func standardToLogicalView(s common.Standard) map[string][]CustomControl {
+	result := make(map[string][]CustomControl)
+	controls := s.GetControls()
+	for _, controlName := range s.GetSortedControls() {
+		control := controls[controlName]
+		_, ok := result[control.GetFamily()]
+		if !ok {
+			result[control.GetFamily()] = make([]CustomControl, 0)
+		}
+		result[control.GetFamily()] = append(result[control.GetFamily()], CustomControl{
+			Key:     controlName,
+			Control: control})
+	}
+	return result
+}
+
+var validImplementationStatuses = []string{"complete", "partial", "not applicable", "planned", "unsatisfied", "unknown", "none"}
+
+func (ms *OpencontrolData) ComponentLogicalView(c common.Component) (map[string]map[string][]CustomControl, []string) {
+	result := make(map[string]map[string][]CustomControl)
+	problems := make([]string, 0)
+
+	for _, satisfy := range c.GetAllSatisfies() {
+		standardKey := satisfy.GetStandardKey()
+		_, ok := result[standardKey]
+		if !ok {
+			standard, found := ms.GetStandard(standardKey)
+			if found {
+				result[standardKey] = standardToLogicalView(standard)
+			}
+
+		}
+		found := false
+		for groupId, group := range result[standardKey] {
+			for i, cc := range group {
+				if cc.Key == satisfy.GetControlKey() {
+					if cc.Satisfies != nil {
+						problems = append(problems, fmt.Sprintf("Found duplicate item: %s", cc.Key))
+					}
+
+					result[standardKey][groupId][i].Satisfies = satisfy
+					found = true
+
+					switch satisfy.GetImplementationStatus() {
+					case "complete", "partial", "not applicable", "planned", "unsatisfied", "unknown", "none":
+						break
+					default:
+						problems = append(problems,
+							fmt.Sprintf("Found non-standard implementation_status: %s. Expected one of %s.",
+								satisfy.GetImplementationStatus(), validImplementationStatuses))
+						break
+					}
+
+					break
+				}
+
+			}
+			if found {
+				break
+			}
+		}
+		if !found {
+			problems = append(problems, fmt.Sprintf("Could not find reference %s in the standard %s", satisfy.GetControlKey(), standardKey))
+
+		}
+	}
+
+	return result, problems
+}
