ff8d85ab9185a5e00129bf002ade7f834180e831
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-25 08:14:47 +0100

    Introduce function to produce CSV based on list of open controls

--- assets/src/app/lib/opencontrol.tsx
+++ assets/src/app/lib/opencontrol.tsx
@@ -24,3 +24,27 @@ export interface Certification {
     Key: string,
     Controls: string[]
 }
+
+export function OpenControlToCSV(list: CustomControl[]) {
+    var result = "NIST-800-53,Name,Description,Implementation Status,Control Response\n"
+    list.map((c) => {
+        const status = c.Satisfies ? c.Satisfies.implementation_status : "unknown"
+        const response = c.Satisfies ?
+                  (Array.isArray(c.Satisfies.narrative) ?
+                   c.Satisfies.narrative.map((n) => { return n.text + '\n'}).reduce((a,b) => a+b) :
+                   c.Satisfies.narrative
+                  ) :
+                  ""
+        const line = c.Key + ","
+                 + c.Control.name + ","
+                 + escape(c.Control.description) + ","
+                 + status + ","
+                 + escape(response) + "\n"
+        result += line
+    })
+    return result
+}
+
+function escape(field) {
+    return '"' + field.replace('"', '""') + '"'
+}
