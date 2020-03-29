0d4309a46cd4c787e871fc39abeb5ae542b4e1da
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-29 20:06:58 +0200

    Implement refresh of historical statistics for each available component

--- pkg/masonry/history_statistics.go
+++ pkg/masonry/history_statistics.go
@@ -1,44 +1,84 @@
 package masonry
 
 import (
-	"fmt"
-	//"github.com/opencontrol/compliance-masonry/pkg/lib/common"
 	"github.com/RedHatGov/ocdb/pkg/git"
+	"sync"
 	"time"
 )
 
 const (
 	// how many months back we want to in the statistics?
-	startingYear  = 2019
-	startingMonth = 12
+	startingYear  = 2020
+	startingMonth = 01
 	ocGit         = "https://github.com/ComplianceAsCode/redhat"
 	ocDir         = "/tmp/.ComplianceAsCode.redhat"
 )
 
-type HistoricalStats struct {
-	Snapshots []SnapshotStats
+type HistoricalStats map[string]ComponentStats
+
+type ComponentStats []ComponentSnapshotStats
+
+type ComponentSnapshotStats struct {
+	time.Time
+	Stats ComponentStatistics
 }
 
-type SnapshotStats struct {
-	time.Time
-	Components map[string]ComponentStatistics
+var hsInstance *HistoricalStats
+var hsMux sync.Mutex
+
+func GetHistoricalStats() *HistoricalStats {
+	if hsInstance == nil {
+		RefreshHistoryStatistics()
+	}
+	return hsInstance
 }
 
 func RefreshHistoryStatistics() error {
+	hsMux.Lock()
+	defer hsMux.Unlock()
+
 	startDate := time.Date(startingYear, time.Month(startingMonth), 1, 0, 0, 0, 0, time.UTC)
 	gitStartDate := startDate.AddDate(0, -1, 0)
 	err := git.PullOrClone(ocDir, ocGit, &(gitStartDate))
 	if err != nil {
 		return err
 	}
+	res := HistoricalStats{}
 	for date := range generateDatesMonthly(startDate) {
 		sha, err := git.LastCommitBy(ocDir, date)
 		if err != nil {
 			return err
 		}
-		fmt.Print(sha)
+		snapshots, err := newSnapshotStats(date, sha)
+		if err != nil {
+			return err
+		}
+		for componentID, snapshot := range snapshots {
+			_, found := res[componentID]
+			if !found {
+				res[componentID] = ComponentStats{}
+			}
+			res[componentID] = append(res[componentID], snapshot)
+		}
 	}
+	hsInstance = &res
 	return nil
+}
+
+func newSnapshotStats(date time.Time, gitSha string) (map[string]ComponentSnapshotStats, error) {
+	res := map[string]ComponentSnapshotStats{}
+	oc, err := newOpencontrolData(gitSha, "/tmp/.ComplianceAsCode.content.rev")
+	if err != nil {
+		return nil, err
+	}
+	for _, component := range oc.GetAllComponents() {
+		res[component.GetKey()] = ComponentSnapshotStats{
+			Time:  date,
+			Stats: oc.ComponentStatistics(component),
+		}
+	}
+
+	return res, nil
 }
 
 func generateDatesMonthly(since time.Time) chan time.Time {
