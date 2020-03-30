e9cb13a743c1b48db56f1c84a1ba6391f60da2e0
Šimon Lukašík <isimluk@fedoraproject.org>
2020-03-30 12:42:37 +0200

    Fill-in stack charts with actual data

--- assets/src/app/ato/Charts/StackCharts.tsx
+++ assets/src/app/ato/Charts/StackCharts.tsx
@@ -72,66 +72,46 @@ interface CompletionStackChartProps {
 }
 
 const CompletionStackChart = React.memo((props: CompletionStackChartProps) => {
-    console.log("CompletionStackChart: ", props.statistics)
+    const statuses = props.statistics.map((s) => Object.keys(s.stats)).flat().filter((value, index, self) => {
+        return self.indexOf(value) === index;
+    })
+
+    const result = statuses.map((status) => {
+        return props.statistics.map((snapshot, k) => {
+            const y = snapshot.stats[status]
+            return { 'name': status, 'x': k, 'y': y == undefined ? 0 : y }
+        })
+    })
+
+    const legendData = statuses.map((status) => {
+        return { name: status }
+    })
 
     return (
         <React.Fragment>
             <p>{props.certName}</p>
-            <div style={{ height: '230px', width: '500px' }}>
+            <div style={{ height: '700px', width: '500px' }}>
                 <Chart
                     ariaDesc="Average number of pets"
                     ariaTitle="Area chart example"
-                    legendData={[{ name: 'Cats' }, { name: 'Birds' }, { name: 'Dogs' }]}
+                    legendData={legendData}
                     legendPosition="bottom-left"
-                    height={225}
+                    height={700}
                     padding={{
                         bottom: 75, // Adjusted to accomodate legend
                         left: 50,
                         right: 50,
                         top: 50,
                     }}
-                    maxDomain={{y: 30}}
+                    maxDomain={{y: 622}}
                     themeColor={ChartThemeColor.multiUnordered}
                 >
                     <ChartAxis />
                     <ChartAxis dependentAxis showGrid />
                     <ChartStack>
-                        <ChartArea
-                            data={[
-                                { name: 'Cats', x: 'Sunday', y: 6 },
-                                { name: 'Cats', x: 'Monday', y: 2 },
-                                { name: 'Cats', x: 'Tuesday', y: 8 },
-                                { name: 'Cats', x: 'Wednesday', y: 15 },
-                                { name: 'Cats', x: 'Thursday', y: 6 },
-                                { name: 'Cats', x: 'Friday', y: 2 },
-                                { name: 'Cats', x: 'Saturday', y: 0 }
-                            ]}
-                            interpolation="monotoneX"
-                        />
-                        <ChartArea
-                            data={[
-                                { name: 'Birds', x: 'Sunday', y: 4 },
-                                { name: 'Birds', x: 'Monday', y: 5 },
-                                { name: 'Birds', x: 'Tuesday', y: 7 },
-                                { name: 'Birds', x: 'Wednesday', y: 6 },
-                                { name: 'Birds', x: 'Thursday', y: 10 },
-                                { name: 'Birds', x: 'Friday', y: 3 },
-                                { name: 'Birds', x: 'Saturday', y: 5 }
-                            ]}
-                            interpolation="monotoneX"
-                        />
-                        <ChartArea
-                            data={[
-                                { name: 'Dogs', x: 'Sunday', y: 8 },
-                                { name: 'Dogs', x: 'Monday', y: 18 },
-                                { name: 'Dogs', x: 'Tuesday', y: 14 },
-                                { name: 'Dogs', x: 'Wednesday', y: 8 },
-                                { name: 'Dogs', x: 'Thursday', y: 6 },
-                                { name: 'Dogs', x: 'Friday', y: 8 },
-                                { name: 'Dogs', x: 'Saturday', y: 12 }
-                            ]}
-                            interpolation="monotoneX"
-                        />
+                        { result.map((statusArea) => {
+                            return (<ChartArea key={statusArea[0].name} data={statusArea} interpolation="monotoneX" />)
+                        }) }
                     </ChartStack>
                 </Chart>
             </div>
