RedHatGov/ocdb

commit a8d669bfaa9649a6d2e87f0421235bc72d4c483e

Making SSP templates product centric, as opposed to full list of prods

assets/src/app/ato/Documents/SSPTemplates.tsx
@@ -1,0 +1,69 @@
+import {
+    Flex, FlexItem,
+    Page, PageSection, PageSectionVariants,
+    Text, TextContent
+} from '@patternfly/react-core';
+import { FileWordIcon } from '@patternfly/react-icons';
+import { Table, TableBody, TableHeader, TableVariant } from '@patternfly/react-table';
+import * as React from 'react';
+
+interface FedRAMPsProps {
+    productID: string;
+}
+
+class ComponentSSPTemplates extends React.Component<FedRAMPsProps> {
+    columns = ['FedRAMP Low', 'FedRAMP Moderate', 'FedRAMP High'];
+
+    buildRows(products) {
+             return [[
+                (<TextContent key={products+'l'}>
+                    <Text component="a" href={'/api/v1/components/' + products + '/fedramp/Low'}><FileWordIcon/> .docx</Text>
+                </TextContent>),
+                (<TextContent key={products+'m'}>
+                    <Text component="a" href={'/api/v1/components/' + products + '/fedramp/Moderate'}><FileWordIcon/> .docx</Text>
+                </TextContent>),
+                (<TextContent key={products+'h'}>
+                    <Text component="a" href={'/api/v1/components/' + products + '/fedramp/High'}><FileWordIcon/> .docx</Text>
+                </TextContent>),
+            ]]
+    }
+
+    render() {
+        const textMod = {modifier: 'flex-2' as 'flex-default', breakpoint: 'md' as 'md'};
+        const emptyMod = {modifier: 'flex-1' as 'flex-default', breakpoint: 'md' as 'md'};
+        return (
+            <Page>
+                <PageSection variant={PageSectionVariants.light}>
+                    <Flex>
+                        <Flex breakpointMods={[textMod]}>
+                            <FlexItem>
+                                <TextContent>
+                                    <Text component="h1">SSP Templates</Text>
+                                    <Text component="p">
+                                        The FedRAMP templates are dynamically generated using the <Text component="a" href="https://github.com/opencontrol/fedramp-templater">OpenControl FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text>. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramproductstemplates/">provided by the GSA FedRAMP PMO</Text>.
+                                    </Text>
+                                </TextContent>
+                            </FlexItem>
+                        </Flex>
+                        <Flex breakpointMods={[emptyMod]}>
+                            <FlexItem/>
+                        </Flex>
+                    </Flex>
+
+                    <TextContent>
+                        <Table
+                            caption="Download Section"
+                            variant={TableVariant.compact}
+                            rows={this.buildRows(this.props.productID)}
+                            cells={this.columns}>
+                            <TableHeader />
+                            <TableBody />
+                        </Table>
+                    </TextContent>
+                </PageSection>
+            </Page>
+        )
+    }
+}
+
+export { ComponentSSPTemplates };

assets/src/app/AppLayout/Navigation.tsx
@@ -43,7 +43,7 @@ const staticNavigation: BaseRoute[] = [
         new ProductRoute('System and Information Integrity', '/ato/products/select/NIST-800-53#SI-'),
     ]),
     new ProductRoute('SCAP', '/ato/products/select/SCAP'),
-    new BasicRoute('SSP Templates', '/ato/fedramp-templates'),
+    new ProductRoute('SSP Templates', '/ato/products/select/ssp-templates'),
     new ProductRoute('Certification Progress', '/ato/products/select/Charts'),
 ];
 

assets/src/app/ato/Documents/Documents.tsx
@@ -5,7 +5,7 @@ import { Flex, FlexItem } from '@patternfly/react-core';
 import VMP from '@app/assets/markdown/vulnerability-management-plan.md';
 import TP from '@app/assets/markdown/training-plan.md';
 import { Markdown } from '@app/lib/markdown';
-import { ComponentFedRAMPTemplates } from '@app/ato/Documents/Fedramp';
+import { ComponentSSPTemplates } from '@app/ato/Documents/SSPTemplates';
 
 const Document: React.FunctionComponent<any> = (props) => {
     var textMod = {modifier: 'flex-2' as 'flex-default', breakpoint: 'md' as 'md'};
@@ -38,4 +38,4 @@ const ATOTrainingPlan: React.FunctionComponent<any> = (props) => {
     return (<Document><TP/></Document>);
 }
 
-export { ATOVulnerabilityManagementPlan, ATOTrainingPlan, ComponentFedRAMPTemplates };
+export { ATOVulnerabilityManagementPlan, ATOTrainingPlan, ComponentSSPTemplates };

assets/src/app/ato/Documents/Fedramp.tsx
@@ -1,85 +1,0 @@
-import * as React from 'react';
-import {
-    Flex, FlexItem,
-    Page, PageSection, PageSectionVariants,
-    Text, TextContent,
-} from '@patternfly/react-core';
-import { FileWordIcon } from '@patternfly/react-icons'
-import { IRow, Table, TableBody, TableHeader, TableVariant } from '@patternfly/react-table'
-import * as Api from '@app/lib/api'
-
-interface FedRAMPsState {
-    rows: IRow[];
-}
-class ComponentFedRAMPTemplates extends React.Component<any, FedRAMPsState> {
-    columns = ['Product', 'FedRAMP Low', 'FedRAMP Moderate', 'FedRAMP High'];
-    constructor(props) {
-        super(props);
-        Api.components()
-           .then(data => this.buildRows(data));
-        this.state = {
-            rows: [],
-        }
-    }
-    buildRows(products) {
-        this.setState({rows: products.filter((p) => {
-                return p.satisfies
-            }).map(function(p, idx) {
-            return [
-                (<Text component="p" key={idx}>{p.name}</Text>),
-                /* (<TextContent key={idx+'t'}>
-                <center>
-                    <Text component="a" href={'/api/v1/components/' + p.key + '/fedramp/Tailored'}><FileWordIcon/> .docx</Text>
-                </center>
-                </TextContent>), */
-                (<TextContent key={idx+'l'}>
-                    <Text component="a" href={'/api/v1/components/' + p.key + '/fedramp/Low'}><FileWordIcon/> .docx</Text>
-                </TextContent>),
-                (<TextContent key={idx+'m'}>
-                    <Text component="a" href={'/api/v1/components/' + p.key + '/fedramp/Moderate'}><FileWordIcon/> .docx</Text>
-                </TextContent>),
-                (<TextContent key={idx+'h'}>
-                    <Text component="a" href={'/api/v1/components/' + p.key + '/fedramp/High'}><FileWordIcon/> .docx</Text>
-                </TextContent>),
-            ];
-        })});
-    }
-    render() {
-        const textMod = {modifier: 'flex-2' as 'flex-default', breakpoint: 'md' as 'md'};
-        const emptyMod = {modifier: 'flex-1' as 'flex-default', breakpoint: 'md' as 'md'};
-        return (
-            <Page>
-                <PageSection variant={PageSectionVariants.light}>
-                    <Flex>
-                        <Flex breakpointMods={[textMod]}>
-                            <FlexItem>
-                                <TextContent>
-                                    <Text component="h1">FedRAMP Templates</Text>
-                                    <Text component="p">
-                                        The FedRAMP template is dynamically generated using the <Text component="a" href="https://github.com/opencontrol/fedramp-templater">OpenControl FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text>. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramp.gov/templates/">provided by the GSA FedRAMP PMO</Text>.
-                                    </Text>
-                                </TextContent>
-                            </FlexItem>
-                        </Flex>
-                        <Flex breakpointMods={[emptyMod]}>
-                            <FlexItem/>
-                        </Flex>
-                    </Flex>
-    
-                    <TextContent>
-                        <Table
-                            caption="Download Section"
-                            variant={TableVariant.compact}
-                            rows={this.state.rows}
-                            cells={this.columns}>
-                            <TableHeader />
-                            <TableBody />
-                        </Table>
-                    </TextContent>
-                </PageSection>
-            </Page>
-        )
-    }
-}
-
-export { ComponentFedRAMPTemplates };

assets/src/app/ato/Products/Product.tsx
@@ -17,6 +17,7 @@ import { RTMDataList } from '@app/ato/Products/DataList.tsx'
 import { Products } from '@app/ato/Products/Products'
 import { Certification } from '@app/lib/opencontrol'
 import { CompletionCharts } from '@app/ato/Charts/CompletionCharts'
+import { ComponentSSPTemplates } from '@app/ato/Documents/SSPTemplates'
 
 interface ProposeChangeProps {
     link: string;
@@ -94,7 +95,11 @@ class Product extends React.PureComponent<any, ProductState> {
     }
 
     showingMarkdown(): boolean {
-        return this.state.activeTabKey != 'NIST-800-53' && this.state.activeTabKey != 'nist-800-53' && !this.showingCharts()
+        return this.state.activeTabKey != 'NIST-800-53' && this.state.activeTabKey != 'nist-800-53' && !this.showingCharts() && !this.showingSSPTemplates()
+    }
+
+    showingSSPTemplates(): boolean {
+        return this.state.activeTabKey == 'ssp-templates'
     }
 
     showingCharts(): boolean {
@@ -117,6 +122,8 @@ class Product extends React.PureComponent<any, ProductState> {
             return (
                 <Text>NIST-800-53 responses for {this.state.product.name} are not available in a form of open controls and/or oscal.</Text>
             )
+        } else if (this.showingSSPTemplates()) {
+            return this.renderSSPTemplates()
         } else if (this.showingCharts()) {
             return this.renderCharts()
         } else {
@@ -174,6 +181,12 @@ class Product extends React.PureComponent<any, ProductState> {
     renderCharts() {
         return (
             <CompletionCharts productId={this.state.productId} />
+        )
+    }
+
+    renderSSPTemplates() {
+        return (
+            <ComponentSSPTemplates productID={this.state.productId} />
         )
     }
 

assets/src/app/routes.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Route, RouteComponentProps, Switch, Redirect } from 'react-router-dom';
 import { accessibleRouteChangeHandler } from '@app/utils/utils';
 import { ATODocuments } from '@app/ato/Documents/Overview';
-import { ATOTrainingPlan, ATOVulnerabilityManagementPlan, ComponentFedRAMPTemplates } from '@app/ato/Documents/Documents';
+import { ATOTrainingPlan, ATOVulnerabilityManagementPlan, ComponentSSPTemplates } from '@app/ato/Documents/Documents';
 import { Product } from '@app/ato/Products/Product';
 import { Products } from '@app/ato/Products/Products';
 import { GettingStarted } from '@app/ato/GettingStarted';
@@ -87,15 +87,6 @@ const routes: IAppRoute[] = [
         path: '/ato/documents/security-awareness-and-training-plan',
         title: 'Security Awareness and Training Plan',
     },
-    {
-        component: ComponentFedRAMPTemplates,
-        exact: true,
-        icon: null,
-        label: 'FedRAMP Templates',
-        path: '/ato/fedramp-templates',
-        title: 'FedRAMP Templates',
-    },
-
   {
     component: Products,
     exact: true,

commit 247c0ebdcb485a1ca9aaefc2e3649301b4838af5

Fix the broken link
It has been broken since a8d669bfaa9649a6d2e87f0421235bc72d4c483e.

assets/src/app/ato/Documents/SSPTemplates.tsx
@@ -53,7 +53,7 @@ class ComponentSSPTemplates extends React.Component<FedRAMPsProps> {
                                 <TextContent>
                                     <Text component="h1">SSP Templates</Text>
                                     <Text component="p">
-                                        The FedRAMP templates are dynamically generated using the <Text component="a" href="https://github.com/gocomply/fedramp">FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text> and then rewritten to adopt OSCAL. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramproductstemplates/">provided by the GSA FedRAMP PMO</Text>.
+                                        The FedRAMP templates are dynamically generated using the <Text component="a" href="https://github.com/gocomply/fedramp">FedRAMP Templater</Text> tool, originally created by <Text component="a" href="https://18f.gsa.gov/">GSA's 18F</Text> and then rewritten to adopt OSCAL. An automated build system incorporates <Text component="a" href="https://github.com/ComplianceAsCode/redhat">Red Hat's OpenControl Content</Text> directly into the FedRAMP Templates <Text component="a" href="https://www.fedramp.gov/templates/">provided by the GSA FedRAMP PMO</Text>.
                                     </Text>
                                 </TextContent>
                             </FlexItem>